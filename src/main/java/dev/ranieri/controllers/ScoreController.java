f2b303e59b3818221bdd20754a55e2c9b6a900fc
AdamRanieri <adaman94@gmail.com>
2022-08-31 15:38:07 -0400

    Added functionality for deleting a score

--- src/main/java/dev/ranieri/controllers/ScoreController.java
+++ src/main/java/dev/ranieri/controllers/ScoreController.java
@@ -4,7 +4,9 @@ import dev.ranieri.entities.Score;
 import dev.ranieri.services.ScoreService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -20,5 +22,20 @@ public class ScoreController {
     public List<Score> allScores(){
         return this.scoreService.getAllScores();
     }
+
+    @DeleteMapping("/scores/{id}")
+    @ResponseBody
+    public String deleteScoreById(@RequestParam String id){
+        int sId = Integer.parseInt(id);
+        boolean isSuccessful = this.scoreService.deleteScoreById(sId);
+        if(isSuccessful){
+            return "Score was deleted";
+        }else{
+            return "FAILURE";
+        }
+
+    }
+
+
 
 }
